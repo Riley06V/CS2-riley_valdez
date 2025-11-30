0bee449c9bf90c1f825bcf25556555bb89c5b991
Riley Valdez <rileyvaldez06@gmail.com>
2025-11-30 12:12:31 -0700

    Added normalization of words

--- Assignments/HW5/main.cpp
+++ Assignments/HW5/main.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+string normalize(string&);
 int main(int argc, char* argv[])
 {
     BST<Word> myTree;
@@ -13,8 +14,29 @@ int main(int argc, char* argv[])
     cin >> filename;
 
     ifstream file(filename);
+    string rawWord;
+
+    while (file >> rawWord) {
+        string word = normalize(rawWord);
+    }
+
+
 
 
     return 0;
 }
 
+string normalize(string &raw) {
+    string cleaned;
+    for (char characters : raw) {
+        // convert uppercase to lowercase
+        if (characters >= 'A' && characters <= 'Z') {
+            characters = characters - 'A' + 'a';
+        }
+        //keep only letters and digits
+        if ((characters >= 'a' && characters <= 'z') || (characters >= '0' && characters <= '9')) {
+            cleaned += characters;
+        }
+    }
+    return cleaned;
+}
