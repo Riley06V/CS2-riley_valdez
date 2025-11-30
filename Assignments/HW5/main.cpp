a562776a4ff69e2d7b72bf3f7c4b4dc603d03405
Riley Valdez <rileyvaldez06@gmail.com>
2025-11-30 10:43:18 -0700

    Copied code from Lab5

---
+++ Assignments/HW5/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "tree.h"
+#include <sstream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+
+    return 0;
+}
+
