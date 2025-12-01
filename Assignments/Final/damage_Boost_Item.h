cc1429b4c25d8f7439faec91c694746af328995e
Riley Valdez <rileyvaldez06@gmail.com>
2025-12-01 13:26:10 -0700

    Adding inheritance to Item class

---
+++ Assignments/Final/damage_Boost_Item.h
@@ -0,0 +1,16 @@
+//
+// Created by riley on 12/1/2025.
+//
+
+#ifndef DAMAGE_BOOST_ITEM_H
+#define DAMAGE_BOOST_ITEM_H
+
+
+
+class damage_Boost_Item {
+
+};
+
+
+
+#endif //DAMAGE_BOOST_ITEM_H
---
+++ Assignments/Final/heal_Item.h
@@ -0,0 +1,16 @@
+//
+// Created by riley on 12/1/2025.
+//
+
+#ifndef HEAL_ITEM_H
+#define HEAL_ITEM_H
+
+
+
+class heal_Item {
+
+};
+
+
+
+#endif //HEAL_ITEM_H
