3c7dc47a681cea3bbebdf8732bc00bff6a979b68
Riley Valdez <rileyvaldez06@gmail.com>
2025-12-01 13:33:39 -0700

    player.h functions

--- Assignments/Final/player.h
+++ Assignments/Final/player.h
@@ -4,11 +4,47 @@
 
 #ifndef PLAYER_H
 #define PLAYER_H
-
+#include <string>
 
 
 class player {
+  private:
+    //player info
+    std::string name;
+    int health;
+    int atkPower;
+    int defense;
+    int inventorySpace;
+    //player position
+    int x;
+    int y;
+  public:
+    //Constructor
+    Player(std::string name, int startX, int startY, int health, int atkPower, int defense);
 
+    //setters
+    void setName();
+    void setHealth();
+    void setAtkPower();
+    void setDefense();
+    void setInventorySpace();
+    void setX();
+    void setY();
+    //getters
+    std::string getName();
+    int getHealth();
+    int getAtkPower();
+    int getDefense();
+    int getInventorySpace();
+    int getX();
+    int getY();
+    //helpers
+    void takeDamage(int damage);
+    void heal(int healAmt);
+    void addItem(const Item &item);
+    void removeItem(const Item &item);
+    void showInventory();
+    bool isAlive();
 };
 
 
